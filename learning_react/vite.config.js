be61f08118a8b3b3714f9c489fc904011597109d
dongsoo.lee <dongsoo.lee@capgemini.com>
2025-04-08 21:26:35 -0400

    path change

--- learning_react/vite.config.js
+++ learning_react/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/sample-web-js-react/learning_react",
+    base: "/sample-web-js-react",
     plugins: [react()],
 })
