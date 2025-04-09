2f0ae7d23c9cca84703c4f48ed0ae31f4b76da18
dongsoo.lee <dongsoo.lee@capgemini.com>
2025-04-08 21:16:07 -0400

    deploy gh page

--- learning_react/vite.config.js
+++ learning_react/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: "/sample-web-js-react/learning_react",
+    plugins: [react()],
 })
